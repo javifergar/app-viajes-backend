cc59d64f8306b0aa3f748b3d37a1695ffcfbd59c
Javier Morales <eyjavi@gmail.com>
2025-11-15 11:44:49 +0100

    conexion a Aiven creada y probada sin errores.
    No propociona resultdos porque la bbdd está vacia.

--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 
 router.use('/viajes', require('./api/viajes.routes'));
+router.use('/usuarios', require('../routes/api/usuarios.routes'));
 
 module.exports = router;
