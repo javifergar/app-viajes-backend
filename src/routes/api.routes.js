2983f7bb6c64f00c2630df0559548fe9927a9914
caverdei <carolinavrdj54@gmail.com>
2025-11-17 00:00:25 +0100

    CRUD participants (solicitudes/participanes viaje)

--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -3,5 +3,6 @@ const router = require('express').Router();
 router.use('/viajes', require('./api/viajes.routes'));
 router.use('/users', require('../routes/api/users.routes'));
 router.use('/ratings', require('./api/ratings.routes'));
+router.use('/participants', require('../routes/api/participants.routes'));
 
 module.exports = router;
--- src/routes/api/miembros.routes.js
+++
@@ -1,5 +0,0 @@
-const router = require('express').Router();
-
-// Define your routes here
-
-module.exports = router;
