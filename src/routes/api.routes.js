3f2ec2315818d8601b607505a584e50673fa3353
Javier Morales <eyjavi@gmail.com>
2025-11-23 10:32:49 +0100

    router auth created and works

--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -1,8 +1,19 @@
+//API routes file
 const router = require('express').Router();
+const trips = require('./api/trips.routes');
+const auth = require('./api/auth.routers');
+const users = require('./api/users.routes');
+const ratings = require('./api/ratings.routes');
+const participants = require('./api/participants.routes');
 
-router.use('/trips', require('./api/trips.routes'));
-router.use('/users', require('../routes/api/users.routes'));
-router.use('/ratings', require('./api/ratings.routes'));
-router.use('/participants', require('./api/participants.routes'));
+//Middlewares
+const { checkToken } = require('../middlewares/auth.middleware');
+
+//Routes
+router.use('/trips', trips);
+router.use('/auth', auth);
+router.use('/users', checkToken, users);
+router.use('/ratings', ratings);
+router.use('/participants', participants);
 
 module.exports = router;
