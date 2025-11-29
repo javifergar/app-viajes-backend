6ecc7754f479a65085c42ab9793c0cf9ffd9fab2
DavidRM <david123ramirez@hotmail.com>
2025-11-29 13:04:12 +0100

    parse to camelCase y documentación funciones
    Se pasan las variables a formato camelCase según requisitos a nivel grupal y se documentan las funciones.

--- src/routes/api/ratings.routes.js
+++ src/routes/api/ratings.routes.js
@@ -1,20 +1,20 @@
 const router = require('express').Router();
-const { listByTrip, listForUser, createRating, updateRating, deleteRating } = require('../../controllers/ratings.controller');
+const { getRatingsByTrip, getRatingsByUserId, createRating, updateRating, deleteRating } = require('../../controllers/ratings.controller');
 const { checkTripExists } = require('../../middlewares/tripValidation.middleware');
 
 // Valoraciones de un viaje concreto
-router.get('/trip/:idTrip', checkTripExists, listByTrip);
+router.get('/trip/:tripId', checkTripExists, getRatingsByTrip);
 
 // Valoraciones recibidas por un usuario
-router.get('/user/:idUser', listForUser);
+router.get('/user/:userId', getRatingsByUserId);
 
 // Crear una valoración tras finalizar un viaje
 router.post('/', checkTripExists, createRating);
 
 // Editar la valoración realizada por el usuario autenticado
-router.patch('/:idRating', updateRating);
+router.patch('/:ratingId', updateRating);
 
 // Eliminar una valoración creada por el usuario autenticado
-router.delete('/:idRating', deleteRating);
+router.delete('/:ratingId', deleteRating);
 
 module.exports = router;
